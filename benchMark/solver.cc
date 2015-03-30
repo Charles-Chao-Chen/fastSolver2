65b90c98a1d6c689ca6690f71f833cab0657fcff
Chao Chen <cchen10@stanford.edu>
2015-03-30 11:25:37 -0700

    hello solver

---
+++ benchMark/solver.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// legion stuff
+#include "legion.h"
+using namespace LegionRuntime::HighLevel;
+
+enum {
+  TOP_LEVEL_TASK_ID = 0,
+};
+
+void top_level_task(const Task *task,
+		    const std::vector<PhysicalRegion> &regions,
+		    Context ctx, HighLevelRuntime *runtime) {  
+ 
+  std::cout << "Hello solver!" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+  // register top level task
+  HighLevelRuntime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
+  HighLevelRuntime::register_legion_task<top_level_task>(
+    TOP_LEVEL_TASK_ID,   /* task id */
+    Processor::LOC_PROC, /* cpu */
+    true,  /* single */
+    false, /* index  */
+    AUTO_GENERATE_ID,
+    TaskConfigOptions(false /*leaf task*/),
+    "master-task"
+  );
+
+  // start legion master task
+  return HighLevelRuntime::start(argc, argv);
+}
