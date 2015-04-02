f662a07240f5053d263f13380f77b5bfc5d902b6
Chao Chen <cchen10@stanford.edu>
2015-04-01 20:55:14 -0700

    current solver interface

--- benchMark/solver.cc
+++ benchMark/solver.cc
@@ -15,18 +15,39 @@ void top_level_task(const Task *task,
 		    const std::vector<PhysicalRegion> &regions,
 		    Context ctx, HighLevelRuntime *runtime) {  
 
-  Matrix b;
-  Matrix U, V, D;
+  // ======= Problem configuration =======
+  // solve: A x = b where A = U * V' + D
+  // =====================================
+  int N = 1<<10;
+  int r = 30;
+  Vector b(N);
+  Matrix U(N, r);
+  Matrix V(N, r);
+  Vector D(N);
 
+  // ================================================
+  // generate random matrices, which could
+  //  potentially be done in parallel
+  // ================================================
   // number of processes, or number of ranks as in MPI
   int nProc = 2;
   b.rand( nProc );
   U.rand( nProc );
   V.rand( nProc );
   D.rand( nProc );
-  
-  // fast solver
-  HMatrix Ah( nProc );
+
+  // ========================================================
+  // fast solver for a simple matrix U * V' + D
+  //  where the off-diagonal blocks are exactly low rank,
+  //  so the solve should be accurate (with round-off errors)
+  // ========================================================
+  // number of cores on each machine
+  int nCore = 8;
+  // number of levels for the (balanced) binary tree
+  int level = 4;
+  // assume one leaf every core for now
+  assert( pow(2,level) == nProc*nCore );
+  HMatrix Ah( nProc, nCore, level );
   Ah.init( Rhs, U, V, D );
   Ah.solve();
 
