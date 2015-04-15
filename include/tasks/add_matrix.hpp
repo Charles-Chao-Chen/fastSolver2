207ebee1395d56ced00898f46ff171bd0e503897
Chao Chen <cchen10@stanford.edu>
2015-04-15 09:59:07 -0700

    implemented matrix addition subtraction task, mainly for debugging purpose

---
+++ include/tasks/add_matrix.hpp
@@ -0,0 +1,34 @@
+#ifndef _add_matrix_hpp
+#define _add_matrix_hpp
+
+#include "legion.h"
+using namespace LegionRuntime::HighLevel;
+
+class AddMatrixTask : public IndexLauncher {
+public:
+  struct TaskArgs {
+    double alpha;
+    double beta;
+    int nrow;
+    int cols;
+  };
+  AddMatrixTask(Domain domain,
+		TaskArgument global_arg,
+		ArgumentMap arg_map,
+		Predicate pred = Predicate::TRUE_PRED,
+		bool must = false,
+		MapperID id = 0,
+		MappingTagID tag = 0);
+  
+  static int TASKID;
+
+  static void register_tasks(void);
+
+public:
+  static void
+  cpu_task(const Task *task,
+	   const std::vector<PhysicalRegion> &regions,
+	   Context ctx, HighLevelRuntime *runtime);
+};
+
+#endif
---
+++ src/tasks/add_matrix.cc
@@ -0,0 +1,71 @@
+#include "add_matrix.hpp"
+#include "ptr_matrix.hpp"
+#include "utility.hpp"
+
+int AddMatrixTask::TASKID;
+
+AddMatrixTask::AddMatrixTask(Domain domain,
+			     TaskArgument global_arg,
+			     ArgumentMap arg_map,
+			     Predicate pred,
+			     bool must,
+			     MapperID id,
+			     MappingTagID tag)
+  
+  : IndexLauncher(TASKID, domain, global_arg,
+		  arg_map, pred, must, id, tag) {}
+
+void AddMatrixTask::register_tasks(void)
+{
+  TASKID = HighLevelRuntime::register_legion_task
+    <AddMatrixTask::cpu_task>(AUTO_GENERATE_ID,
+			    Processor::LOC_PROC, 
+			    false,
+			    true,
+			    AUTO_GENERATE_ID,
+			    TaskConfigOptions(true/*leaf*/),
+			    "Leaf_Solve");
+
+#ifdef SHOW_REGISTER_TASKS
+  printf("Register task %d : Leaf_Solve\n", TASKID);
+#endif
+}
+
+void AddMatrixTask::cpu_task(const Task *task,
+			     const std::vector<PhysicalRegion> &regions,
+			     Context ctx, HighLevelRuntime *runtime) {
+
+  assert(regions.size() == 3);
+  assert(task->regions.size() == 3);
+  assert(task->arglen == sizeof(TaskArgs));
+
+  Point<1> p = task->index_point.get_point<1>();
+  printf("point = %d\n", p[0]);
+
+  const TaskArgs args = *((const TaskArgs*)task->args);
+  int rblk = args.nrow;
+  int cols = args.cols;
+  double alpha = args.alpha;
+  double beta  = args.beta;
+
+  int rlo = p[0]*rblk;
+  int rhi = (p[0] + 1) * rblk;
+
+  /*
+  double *Aptr = region_pointer(regions[0], rlo, rhi, 0, cols);
+  double *Bptr = region_pointer(regions[1], rlo, rhi, 0, cols);
+  double *Cptr = region_pointer(regions[2], rlo, rhi, 0, cols);
+  
+  PtrMatrix AMat(rblk, cols, rblk, Aptr);
+  PtrMatrix BMat(rblk, cols, rblk, Bptr);
+  PtrMatrix CMat(rblk, cols, rblk, Cptr);
+  */
+  
+  PtrMatrix AMat = get_raw_pointer(regions[0], rlo, rhi, 0, cols);
+  PtrMatrix BMat = get_raw_pointer(regions[1], rlo, rhi, 0, cols);
+  PtrMatrix CMat = get_raw_pointer(regions[2], rlo, rhi, 0, cols);
+  PtrMatrix::add(alpha, AMat, beta, BMat, CMat);
+}
+
+
+
