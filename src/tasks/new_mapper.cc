67d501f37a2d344d0a3126f2579f48420780e917
Chao Chen <cchen10@stanford.edu>
2016-05-30 16:29:11 -0700

    updated mapper because of Legion updates

--- src/tasks/new_mapper.cc
+++ src/tasks/new_mapper.cc
@@ -15,17 +15,18 @@ SolverMapper::SolverMapper(Machine machine, Runtime *rt, Processor local,
     sysmems_list(*_sysmems_list),
     sysmem_local_procs(*_sysmem_local_procs),
     proc_sysmems(*_proc_sysmems),
-    proc_regmems(*_proc_regmems)
+  proc_regmems(*_proc_regmems)
 {}
 
 
 void SolverMapper::map_must_epoch(const MapperContext           ctx,
-				  const MapMustEpochInput&      input,
-				  MapMustEpochOutput&     output)
+				   const MapMustEpochInput&      input,
+				   MapMustEpochOutput&     output)
 {
   log_solver_mapper.spew("Default map_must_epoch in %s", get_mapper_name());
   // Figure out how to assign tasks to CPUs first. We know we can't
   // do must epochs for anthing but CPUs at the moment.
+  std::map<const Task*,Processor> proc_map;
   if (total_nodes > 1)
     {
       unsigned tasks_per_sysmem = (input.tasks.size() + sysmems_list.size() - 1) / sysmems_list.size();
@@ -43,6 +44,7 @@ void SolverMapper::map_must_epoch(const MapperContext           ctx,
 	  //task->additional_procs.clear();
 	}
     }
+
   else
     {
       if (input.tasks.size() > local_cpus.size())
@@ -54,103 +56,89 @@ void SolverMapper::map_must_epoch(const MapperContext           ctx,
           assert(false);
         }
       for (unsigned idx = 0; idx < input.tasks.size(); idx++)
-	output.task_processors[idx] = local_cpus[idx];
+        {
+          output.task_processors[idx] = local_cpus[idx];
+          proc_map[input.tasks[idx]] = local_cpus[idx];
+        }
     }
-
-  // Now let's map all the constraints first, and then we'll call map
-  // task for all the tasks and tell it that we already premapped the
-  // constrainted instances
+  // Now let's map the constraints, find one requirement to use for
+  // mapping each of the constraints, but get the set of fields we
+  // care about and the set of logical regions for all the requirements
   for (unsigned cid = 0; cid < input.constraints.size(); cid++)
     {
       const MappingConstraint &constraint = input.constraints[cid];
-      std::vector<Legion::Mapping::PhysicalInstance> &constraint_mapping =
+      std::vector<Legion::Mapping::PhysicalInstance> &constraint_mapping = 
 	output.constraint_mappings[cid];
-      int index1 = -1, index2 = -1;
-      for (unsigned idx = 0; (idx < input.tasks.size()) &&
-	     ((index1 == -1) || (index2 == -1)); idx++)
+      // Figure out which task and region requirement to use as the 
+      // basis for doing the mapping
+      Task *base_task = NULL;
+      unsigned base_index = 0;
+      Processor base_proc = Processor::NO_PROC;
+      std::set<LogicalRegion> needed_regions;
+      std::set<FieldID> needed_fields;
+      for (unsigned idx = 0; idx < constraint.constrained_tasks.size(); idx++)
         {
-          if (constraint.t1 == input.tasks[idx])
-            index1 = idx;
-          if (constraint.t2 == input.tasks[idx])
-            index2 = idx;
-        }
-      assert((index1 >= 0) && (index2 >= 0));
-      // Figure out which memory to use
-      // TODO: figure out how to use registered memory in the multi-node case
-      Memory target1 = default_policy_select_target_memory(ctx,
-							   output.task_processors[index1]);
-      Memory target2 = default_policy_select_target_memory(ctx,
-							   output.task_processors[index2]);
-      // Pick our target memory
-      Memory target_memory = Memory::NO_MEMORY;
-      if (target1 != target2)
-        {
-          // See if one of them is not no access so we can pick the other
-          if (constraint.t1->regions[constraint.idx1].is_no_access())
-            target_memory = target2;
-          else if (constraint.t2->regions[constraint.idx2].is_no_access())
-            target_memory = target1;
-          else
+          Task *task = constraint.constrained_tasks[idx];
+          unsigned req_idx = constraint.requirement_indexes[idx];
+          if ((base_task == NULL) && (!task->regions[req_idx].is_no_access()))
 	    {
-	      log_solver_mapper.error("Default mapper error. Unable to pick a common "
-			       "memory for tasks %s (ID %lld) and %s (ID %lld) "
-			       "in a must epoch launch. This will require a "
-			       "custom mapper.", constraint.t1->get_task_name(),
-			       constraint.t1->get_unique_id(), 
-			       constraint.t2->get_task_name(), 
-			       constraint.t2->get_unique_id());
+	      base_task = task;
+	      base_index = req_idx;
+	      base_proc = proc_map[task];
+	    }
+          needed_regions.insert(task->regions[req_idx].region);
+          needed_fields.insert(task->regions[req_idx].privilege_fields.begin(),
+                               task->regions[req_idx].privilege_fields.end());
+        }
+      // If there wasn't a region requirement that wasn't no access just 
+      // pick the first one since this case doesn't make much sense anyway
+      if (base_task == NULL)
+        {
+          base_task = constraint.constrained_tasks[0];
+          base_index = constraint.requirement_indexes[0];
+          base_proc = proc_map[base_task];
+        }
+      Memory target_memory = default_policy_select_target_memory(ctx, 
+								 base_proc);
+      VariantInfo info = default_find_preferred_variant(*base_task, ctx, 
+							true/*needs tight bound*/, true/*cache*/, Processor::LOC_PROC);
+      const TaskLayoutConstraintSet &layout_constraints = 
+	runtime->find_task_layout_constraints(ctx, base_task->task_id, 
+					      info.variant);
+      if (needed_regions.size() == 1)
+        {
+          // If there was just one region we can use the base region requirement
+          if (!default_create_custom_instances(ctx, base_proc, target_memory,
+					       base_task->regions[base_index], base_index, needed_fields,
+					       layout_constraints, true/*needs check*/, constraint_mapping))
+	    {
+	      log_solver_mapper.error("Default mapper error. Unable to make instance(s) "
+			       "in memory " IDFMT " for index %d of constrained "
+			       "task %s (ID %lld) in must epoch launch.",
+			       target_memory.id, base_index,
+			       base_task->get_task_name(), 
+			       base_task->get_unique_id());
 	      assert(false);
 	    }
         }
-      else // both the same so this is easy
-	target_memory = target1;
-      assert(target_memory.exists());
-      // Figure out the variants that are going to be used by the two tasks    
-      VariantInfo info1 = find_preferred_variant(*constraint.t1, ctx,
-						 true/*needs tight bound*/, Processor::LOC_PROC);
-      VariantInfo info2 = find_preferred_variant(*constraint.t2, ctx,
-						 true/*needs tight_bound*/, Processor::LOC_PROC);
-      // Map it the one way and filter the other so that we can make sure
-      // that they are both going to use the same instance
-      std::set<FieldID> needed_fields = 
-	constraint.t1->regions[constraint.idx1].privilege_fields;
-      needed_fields.insert(
-			   constraint.t2->regions[constraint.idx2].privilege_fields.begin(),
-			   constraint.t2->regions[constraint.idx2].privilege_fields.end());
-      const TaskLayoutConstraintSet &layout_constraints1 = 
-	runtime->find_task_layout_constraints(ctx, 
-						     constraint.t1->task_id, info1.variant);
-      if (!default_create_custom_instances(ctx, 
-					   output.task_processors[index1], target_memory,
-					   constraint.t1->regions[constraint.idx1], constraint.idx1,
-					   needed_fields, layout_constraints1, true/*needs check*/,
-					   constraint_mapping))
+      else
         {
-          log_solver_mapper.error("Default mapper error. Unable to make instance(s) "
-                           "in memory " IDFMT " for index %d of constrained "
-                           "task %s (ID %lld) in must epoch launch.",
-                           target_memory.id, constraint.idx1, 
-                           constraint.t1->get_task_name(),
-                           constraint.t1->get_unique_id());
-          assert(false);
-        }
-      // Copy the results over and make sure they are still good 
-      const size_t num_instances = constraint_mapping.size();
-      assert(num_instances > 0);
-      std::set<FieldID> missing_fields;
-      runtime->filter_instances(ctx, *constraint.t2, constraint.idx2,
-				       info2.variant, constraint_mapping, missing_fields);
-      if (num_instances != constraint_mapping.size())
-        {
-          log_solver_mapper.error("Default mapper error. Unable to make instance(s) "
-                           "for index %d of constrained task %s (ID %lld) in "
-                           "must epoch launch. Most likely this is because "
-                           "conflicting constraints are requested for regions "
-                           "which must be mapped to the same instance. You "
-                           "will need to write a custom mapper to fix this.",
-                           constraint.idx2, constraint.t2->get_task_name(),
-                           constraint.t2->get_unique_id());
-          assert(false);
+          // Otherwise we need to find a common region that will satisfy all
+          // the needed regions
+          RegionRequirement copy_req = base_task->regions[base_index];
+          copy_req.region = default_find_common_ancestor(ctx, needed_regions);
+          if (!default_create_custom_instances(ctx, base_proc, target_memory,
+					       copy_req, base_index, needed_fields, layout_constraints,
+					       true/*needs check*/, constraint_mapping))
+	    {
+	      log_solver_mapper.error("Default mapper error. Unable to make instance(s) "
+			       "in memory " IDFMT " for index %d of constrained "
+			       "task %s (ID %lld) in must epoch launch.",
+			       target_memory.id, base_index,
+			       base_task->get_task_name(), 
+			       base_task->get_unique_id());
+	      assert(false);
+	    }
         }
     }
 }
