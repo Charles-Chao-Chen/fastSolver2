17ce5cedcb867596a870fcc5c43d492568c9abda
Chao Chen <chenchao.nk@gmail.com>
2016-09-26 09:52:15 +0200

    small changes

--- spmd_benchMark/make_job.sh
+++ spmd_benchMark/make_job.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-NumNodes=4
+NumNodes=512
 NumCore=4
-Mtxlvl=6
+Mtxlvl=17
 #Time=00:05:00
 
 export NumProcs
 echo Run legion solver at `echo $NumNodes` nodes...
 sbatch --nodes=${NumNodes} \
 	--ntasks=${NumNodes} \
-	--time=00:01:00 job_solver.slm \
+	--time=00:05:00 job_solver.slm \
 	${NumNodes} ${NumCore} ${Mtxlvl}
