4752cf6f65f882507b3ba51258cc871240be8454
Chao Chen <cchen10@stanford.edu>
2016-10-17 23:23:55 -0700

    updated run_solver.sh

--- spmd_benchMark/run_solver.sh
+++ spmd_benchMark/run_solver.sh
@@ -3,9 +3,11 @@
 Nn=1
 Ofile=$Nn"node_%.log"
 
+#	-x GASNET_IB_SPAWNER=mpi  \
+#	-level inst=2,metadata=2 \
+
 mpirun -np $Nn -pernode \
 	-bind-to none -report-bindings \
-	-x GASNET_IB_SPAWNER=mpi  \
 	-x GASNET_BACKTRACE=1     \
 	-x LEGION_FREEZE_ON_ERROR=1 \
 	./solver \
@@ -13,5 +15,6 @@ mpirun -np $Nn -pernode \
 	-core 8 \
 	-mtxlvl 6 \
 	-ll:cpu 8 \
-	-level inst=2,metadata=2 \
+	-hl:prof $Nn \
+	-level 4,legion_prof=2 \
 	-logfile $Ofile
