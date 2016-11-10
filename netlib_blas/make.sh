3a4b6f03b7abd35133414b67d376361e5ed2e8d1
Elliott Slaughter <slaughter@cs.stanford.edu>
2016-11-10 18:47:57 -0500

    Add vendored Netlib BLAS and LAPACK libraries.

---
+++ netlib_blas/make.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+for file in *.f; do ftn -c $file; done
+gcc main.cc dgemm.o  dgesv.o  dgetrf.o  dgetrf2.o  dgetrs.o  dlaswp.o  dtrsm.o  xerbla.o lsame.o ilaenv.o idamax.o dlamch.o dscal.o ieeeck.o iparmq.o -lm -lgfortran -o main
