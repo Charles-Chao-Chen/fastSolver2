3f654decca858613f525e324f0d5eb1bb39636b0
Elliott Slaughter <slaughter@cs.stanford.edu>
2016-11-11 18:38:24 -0500

    Update build script for vendored BLAS.

--- netlib_blas/make.sh
+++ netlib_blas/make.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-for file in *.f; do ftn -O3 -c $file; done
-gcc main.cc dgemm.o  dgesv.o  dgetrf.o  dgetrf2.o  dgetrs.o  dlaswp.o  dtrsm.o  xerbla.o lsame.o ilaenv.o idamax.o dlamch.o dscal.o ieeeck.o iparmq.o -lm -lgfortran -o main
+CC=gcc
+FC=ftn
+FFLAGS="-O3 -march=native"
+
+for file in *.f; do $FC $FFLAGS -c $file; done
+
+# This is to sanity check that we can successfully link all these files.
+$CC main.cc dgemm.o  dgesv.o  dgetrf.o  dgetrf2.o  dgetrs.o  dlaswp.o  dtrsm.o  xerbla.o lsame.o ilaenv.o idamax.o dlamch.o dscal.o ieeeck.o iparmq.o -lm -lgfortran -o main
