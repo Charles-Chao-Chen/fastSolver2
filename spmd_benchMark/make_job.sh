3a935d12a8a5e594ea2decf68d674313510acfc1
Chao Chen <chenchao.nk@gmail.com>
2017-02-01 05:58:33 +0100

    small changes in the slurm script

--- spmd_benchMark/make_job.sh
+++ spmd_benchMark/make_job.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-NumNodes=512
-NumCore=4
-Mtxlvl=17
-#Time=00:05:00
+NumNodes=1 #512
+NumCore=8
+Mtxlvl=8 #17
+Time=00:05:00
 
-export NumProcs
+#export NumProcs
 echo Run legion solver at `echo $NumNodes` nodes...
 sbatch --nodes=${NumNodes} \
-	--ntasks=${NumNodes} \
-	--time=00:05:00 job_solver.slm \
-	${NumNodes} ${NumCore} ${Mtxlvl}
+	--cpus-per-task=${NumCore} \
+	--time=${Time} job_solver.slm \
+	${Mtxlvl}
+
+
+#	--ntasks=${NumNodes} \
+
