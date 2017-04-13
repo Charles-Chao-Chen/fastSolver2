11608ff02c4d39d18322c0e45a4547e4ea6837e0
Chao Chen <cchen10@stanford.edu>
2017-04-13 11:38:49 -0700

    rename the job script on Certainty

--- spmd_benchMark/run_solver.sh
+++ spmd_benchMark/run_solver.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-Nn=2
+Nn=128
+TreeLevel=18
+
 Ofile=$Nn"node_%.log"
 
 #	-x GASNET_IB_SPAWNER=mpi  \
 #	-level inst=2,metadata=2 \
 
 mpirun -np $Nn -pernode \
-	-bind-to none -report-bindings \
+	-bind-to none \
 	-x GASNET_BACKTRACE=1     \
 	-x LEGION_FREEZE_ON_ERROR=1 \
 	./solver \
 	-machine $Nn \
 	-core 8 \
-	-mtxlvl 6 \
+	-mtxlvl $TreeLevel \
 	-ll:cpu 8 \
-	-hl:prof $Nn \
+	-ll:csize 32000 \
+	-hl:sched 1024 \
+	-hl:prof 8 \
 	-level 4,legion_prof=2 \
-	-logfile $Ofile
+	-logfile $Ofile &
